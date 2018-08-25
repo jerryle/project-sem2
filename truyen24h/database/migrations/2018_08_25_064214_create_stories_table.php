d811d0503a7249a01de9aba92fe9cc0ce0c8800e
jerryle <16341051+jerryle@users.noreply.github.com>
2018-08-25 14:13:51 +0700

    init branch

---
+++ truyen24h/database/migrations/2018_08_25_064214_create_stories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',191)->default('None')->unique();
+            $table->string('details',500)->default('None');
+            $table->string('image',255)->nullable();
+            $table->unsignedInteger('uploadedBy');
+            $table->foreign('uploadedBy')->references('id')->on('users');
+            $table->string('author', 128)->nullable();
+            $table->unsignedInteger('view_count')->default(0);
+            $table->unsignedInteger('sub_count')->default(0);
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
