7e87670a669d20ceb4cbd6d5421630a007bc76da
jerryle <16341051+jerryle@users.noreply.github.com>
2018-08-25 15:16:14 +0700

    init branch

---
+++ truyen24h/app/Groups.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Truyen24h;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Groups extends Model
+{
+    //
+}
---
+++ truyen24h/database/migrations/2018_08_25_081320_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',191);
+            $table->string('description', 255);
+            $table->string('image', 255);
+            $table->unsignedInteger('ownerId');
+            $table->foreign('ownerId')->references('id')->on('users');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
