982de297d3bf0a11ff9ba7d77cdcc1a245117f91
jerryle <16341051+jerryle@users.noreply.github.com>
2018-08-29 00:07:01 +0700

    conflict check

--- truyen24h/app/Story.php
+++ truyen24h/app/Story.php
@@ -7,19 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Story extends Model
 {
 
-
-    public function chapters()
-    {
-        return $this->hasMany('Truyen24h\Chapter');
-    }
-
-    public function genres()
-    {
-        return $this->belongsToMany('Truyen24h\Genre');
-    }
-
-    public function user()
-    {
-        return $this->belongsTo('Truyen24h\User');
-    }
 }
