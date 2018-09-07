176e25e725b11a9765c881ddeede2a1b5e9669fd
ExamLaravel <noiltd00616@fpt.edu.vn>
2018-09-07 11:15:53 +0700

    reup

--- truyen24h/resources/views/auth/login.blade.php
+++ truyen24h/resources/views/auth/login.blade.php
@@ -1,104 +1,177 @@
 @extends('layouts.master') @section('content')
-<div class="container my-6">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <section class="form-gradient">
-                <div class="card">
-                    <div class="header pt-3 blue-gradient">
+    <div class="container my-6">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <section class="form-gradient">
+                    <!-- Material form login -->
+                    <div class="card">
 
-                        <div class="row d-flex justify-content-center">
-                            <h3 class="h3-reponsive white-text mb-3 pt-3 font-weight-bold text-uppercase">{{ __('auth.login') }}</h3>
-                        </div>
+                        <h5 class="card-header info-color white-text text-center py-4">
+                            <strong>Đăng Nhập</strong>
+                        </h5>
 
-                        {{--
-                        <div class="row mt-2 mb-3 d-flex justify-content-center">
-                            <!--Facebook-->
-                            <a class="fa-lg p-2 m-2 fb-ic">
-                                <i class="fa fa-facebook white-text fa-lg"> </i>
-                            </a>
-                            <!--Twitter-->
-                            <a class="fa-lg p-2 m-2 tw-ic">
-                                <i class="fa fa-twitter white-text fa-lg"> </i>
-                            </a>
-                            <!--Google +-->
-                            <a class="fa-lg p-2 m-2 gplus-ic">
-                                <i class="fa fa-google-plus white-text fa-lg"> </i>
-                            </a>
-                        </div> --}}
+                        <!--Card content-->
+                        <div class="card-body px-lg-5 pt-0">
 
-
-                    </div>
-
-                    <div class="card-body mx-4 mt-4">
-                        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('auth.login') }}">
+                            <!-- Form -->
+                            <form method="POST" action="{{ route('login') }}" class="text-center"
+                                  style="color: #757575;">
                             @csrf
-                            <!--Body-->
-                            <div class="md-form">
-                                {{--
-                                <input type="text" id="Form-email3" class="form-control"> --}}
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username"
-                                    value="{{ old('username') }}" required autofocus> @if ($errors->has('username'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('username') }}</strong>
-                                </span>
-                                @endif
-                                <label for="username">{{ __('auth.username') }}</label>
-
-                            </div>
-
-                            <div class="md-form pb-1 pb-md-3">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                                    required> @if ($errors->has('password'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                                @endif
-                                <label for="password">{{ __('auth.password') }}</label>
-
-                                <div class="form-check my-4">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('auth.rememberme') }}
-                                    </label>
+                            <!-- Email -->
+                                <div class="md-form">
+                                    <input id="username" type="text"
+                                           class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}"
+                                           name="username"
+                                           value="{{ old('username') }}" required
+                                           autofocus> @if ($errors->has('username'))
+                                        <span class="invalid-feedback" role="alert">
+    <strong>{{ $errors->first('username') }}</strong>
+    </span>
+                                    @endif
+                                    <label for="username">{{ __('auth.username') }}</label>
                                 </div>
 
-                                <p class="font-small grey-text d-flex justify-content-end">{{__('auth.forgot')}}
-                                    <a href="{{ route('password.request') }}" class="dark-grey-text ml-1 font-weight-bold">
-                                    {{__('auth.password')}}</a>?</p>
+                                <!-- Password -->
+                                <div class="md-form">
+                                    <input id="password" type="password"
+                                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                           name="password"
+                                           required> @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+    <strong>{{ $errors->first('password') }}</strong>
+    </span>
+                                    @endif
+                                    <label for="password">{{ __('auth.password') }}</label>
+                                </div>
 
+                                <div class="d-flex justify-content-around">
+                                    <div>
+                                        <!-- Remember me -->
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember"
+                                                   id="remember" {{ old( 'remember') ? 'checked' : '' }}>
 
-                            </div>
-
-
-                            <!--Grid row-->
-                            <div class="row d-flex align-items-center mb-4">
-
-                                <!--Grid column-->
-                                <div class="col-md-1 col-md-5 d-flex align-items-start">
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-grey btn-rounded z-depth-1a">{{ __('auth.login') }}</button>
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('auth.rememberme') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <!-- Forgot password -->
+                                        <a href="{{ route('password.request') }}">Quên mật khẩu?</a>
                                     </div>
                                 </div>
-                                <!--Grid column-->
 
-                                <!--Grid column-->
-                                <div class="col-md-7">
-                                    <p class="font-small grey-text d-flex justify-content-end mt-3">{{ __('auth.user-new') }}
-                                        <a href="{{route('register')}}" class="dark-grey-text ml-1 font-weight-bold">
-                                        {{ __('auth.register') }}</a>
-                                    </p>
-                                </div>
-                                <!--Grid column-->
+                                <!-- Sign in button -->
+                                <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
+                                        type="submit"><strong>Đăng Nhập</strong>
+                                </button>
 
-                            </div>
-                            <!--Grid row-->
-                        </form>
+                                <!-- Register -->
+                                <p>Chưa có tài khoản?
+                                    <a href="{{route('register')}}">Đăng ký</a>
+                                </p>
+
+                            </form>
+                            <!-- Form -->
+
+                        </div>
+
                     </div>
-                </div>
-            </section>
-
+                    <!-- Material form login -->
+                </section>
+            </div>
         </div>
     </div>
-</div>
+
+    {{--<div class="container my-6">--}}
+    {{--<div class="row justify-content-center">--}}
+    {{--<div class="col-md-8">--}}
+    {{--<section class="form-gradient">--}}
+    {{--<div class="card">--}}
+    {{--<div class="header pt-3 purple-gradient">--}}
+
+    {{--<div class="row d-flex justify-content-center">--}}
+    {{--<h3 class="h3-reponsive white-text mb-3 pt-3 font-weight-bold text-uppercase">{{ __('auth.login') }}</h3>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="card-body mx-4 mt-4">--}}
+    {{--<form method="POST" action="{{ route('login') }}" aria-label="{{ __('auth.login') }}">--}}
+    {{--@csrf--}}
+    {{--<!--Body-->--}}
+    {{--<div class="md-form">--}}
+    {{--<input id="username" type="text"--}}
+    {{--class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}"--}}
+    {{--name="username"--}}
+    {{--value="{{ old('username') }}" required--}}
+    {{--autofocus> @if ($errors->has('username'))--}}
+    {{--<span class="invalid-feedback" role="alert">--}}
+    {{--<strong>{{ $errors->first('username') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--<label for="username">{{ __('auth.username') }}</label>--}}
+
+    {{--</div>--}}
+
+    {{--<div class="md-form pb-1 pb-md-3">--}}
+    {{--<input id="password" type="password"--}}
+    {{--class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"--}}
+    {{--name="password"--}}
+    {{--required> @if ($errors->has('password'))--}}
+    {{--<span class="invalid-feedback" role="alert">--}}
+    {{--<strong>{{ $errors->first('password') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--<label for="password">{{ __('auth.password') }}</label>--}}
+
+    {{--<div class="form-check my-4">--}}
+    {{--<input class="form-check-input" type="checkbox" name="remember"--}}
+    {{--id="remember" {{ old( 'remember') ? 'checked' : '' }}>--}}
+
+    {{--<label class="form-check-label" for="remember">--}}
+    {{--{{ __('auth.rememberme') }}--}}
+    {{--</label>--}}
+    {{--</div>--}}
+
+    {{--<p class="font-small grey-text d-flex justify-content-end">{{__('auth.forgot')}}--}}
+    {{--<a href="{{ route('password.request') }}"--}}
+    {{--class="dark-grey-text ml-1 font-weight-bold">--}}
+    {{--{{__('auth.password')}}</a>?</p>--}}
+
+
+    {{--</div>--}}
+
+
+    {{--<!--Grid row-->--}}
+    {{--<div class="row d-flex align-items-center mb-4">--}}
+
+    {{--<!--Grid column-->--}}
+    {{--<div class="col-md-1 col-md-5 d-flex align-items-start">--}}
+    {{--<div class="text-center">--}}
+    {{--<button type="submit"--}}
+    {{--class="btn btn-grey btn-rounded z-depth-1a">{{ __('auth.login') }}</button>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<!--Grid column-->--}}
+
+    {{--<!--Grid column-->--}}
+    {{--<div class="col-md-7">--}}
+    {{--<p class="font-small grey-text d-flex justify-content-end mt-3">{{ __('auth.user-new') }}--}}
+    {{--<a href="{{route('register')}}"--}}
+    {{--class="dark-grey-text ml-1 font-weight-bold">--}}
+    {{--{{ __('auth.register') }}</a>--}}
+    {{--</p>--}}
+    {{--</div>--}}
+    {{--<!--Grid column-->--}}
+
+    {{--</div>--}}
+    {{--<!--Grid row-->--}}
+    {{--</form>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</section>--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 @endsection
--- truyen24h/resources/views/auth/register.blade.php
+++ truyen24h/resources/views/auth/register.blade.php
@@ -1,105 +1,173 @@
 @extends('layouts.master') @section('content')
-<div class="container my-6">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <section class="form-gradient">
-                <div class="card">
-                    <div class="header pt-3 blue-gradient">
+    <div class="container my-6">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <section class="form-gradient">
+                    <!-- Material form register -->
+                    <div class="card">
 
-                        <div class="row d-flex justify-content-center">
-                            <h3 class="h3-reponsive white-text mb-3 pt-3 font-weight-bold text-uppercase">{{ __('auth.register') }}</h3>
-                        </div>
+                        <h5 class="card-header info-color white-text text-center py-4">
+                            <strong>Đăng Ký</strong>
+                        </h5>
 
-                        {{--
-                        <div class="row mt-2 mb-3 d-flex justify-content-center">
-                            <!--Facebook-->
-                            <a class="fa-lg p-2 m-2 fb-ic">
-                                <i class="fa fa-facebook white-text fa-lg"> </i>
-                            </a>
-                            <!--Twitter-->
-                            <a class="fa-lg p-2 m-2 tw-ic">
-                                <i class="fa fa-twitter white-text fa-lg"> </i>
-                            </a>
-                            <!--Google +-->
-                            <a class="fa-lg p-2 m-2 gplus-ic">
-                                <i class="fa fa-google-plus white-text fa-lg"> </i>
-                            </a>
-                        </div> --}}
+                        <!--Card content-->
+                        <div class="card-body px-lg-5 pt-0">
 
-
-                    </div>
-
-                    <div class="card-body mx-4 mt-4">
-                        <form method="POST" action="{{ route('register') }}" aria-label="{{ __('auth.register') }}">
-                            @csrf
-                            <!--Body-->
-                            <div class="md-form">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username"
-                                    required autofocus> @if ($errors->has('username'))
-                                <span class="invalid-feedback" role="alert">
+                            <!-- Form -->
+                            <form method="POST" action="{{ route('register') }}" class="text-center"
+                                  style="color: #757575;">
+                                @csrf
+                                <div class="md-form">
+                                    <input id="username" type="text"
+                                           class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}"
+                                           name="username"
+                                           required autofocus> @if ($errors->has('username'))
+                                        <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('username') }}</strong>
                                 </span>
-                                @endif
-                                <label for="username">{{ __('auth.username') }}</label>
-                            </div>
+                                    @endif
+                                    <label for="username">{{ __('auth.username') }}</label>
+                                </div>
 
-                            <div class="md-form">
-                                {{--
-                                <input type="text" id="Form-email3" class="form-control"> --}}
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
-                                    required> @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
+                                <!-- E-mail -->
+                                <div class="md-form mt-0">
+                                    <input id="email" type="email"
+                                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                           name="email" value="{{ old('email') }}"
+                                           required> @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('email') }}</strong>
                                 </span>
-                                @endif
-                                <label for="email">{{ __('auth.email') }}</label>
+                                    @endif
+                                    <label for="email">{{ __('auth.email') }}</label>
+                                </div>
 
-                            </div>
-
-                            <div class="md-form">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                                    required> @if ($errors->has('password'))
-                                <span class="invalid-feedback" role="alert">
+                                <!-- Password -->
+                                <div class="md-form">
+                                    <input id="password" type="password"
+                                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                           name="password"
+                                           required> @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('password') }}</strong>
                                 </span>
-                                @endif
-                                <label for="password">{{ __('auth.password') }}</label>
-                            </div>
-
-                            <div class="md-form">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                                <label for="password-confirm">{{ __('auth.password-confirm') }}</label>
-
-                            </div>
-
-
-                            <!--Grid row-->
-                            <div class="row d-flex align-items-center mb-4">
-
-                                <!--Grid column-->
-                                <div class="col-md-1 col-md-5 d-flex align-items-start">
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-grey btn-rounded z-depth-1a">{{ __('auth.register-btn') }}</button>
-                                    </div>
+                                    @endif
+                                    <label for="password">{{ __('auth.password') }}</label>
                                 </div>
-                                <!--Grid column-->
 
-                                <!--Grid column-->
-                                <div class="col-md-7">
-                                    <p class="font-small grey-text d-flex justify-content-end mt-3">{{__('auth.user-exists')}}
-                                        <a href="{{route('login')}}" class="dark-grey-text ml-1 font-weight-bold">
-                                        {{ __('auth.login') }}</a>
-                                    </p>
+                                <!-- Phone number -->
+                                <div class="md-form">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                           name="password_confirmation" required>
+                                    <label for="password-confirm">{{ __('auth.password-confirm') }}</label>
                                 </div>
-                                <!--Grid column-->
 
-                            </div>
-                            <!--Grid row-->
-                        </form>
+                                <!-- Sign up button -->
+                                <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
+                                        type="submit"><strong>Đăng ký</strong>
+                                </button>
+                                <p>Đã có tài khoản rồi?
+                                    <a href="{{route('login')}}" target="_blank"> Đăng nhập</a>.</p>
+
+                            </form>
+                            <!-- Form -->
+
+                        </div>
+
                     </div>
-                </div>
-            </section>
+                    <!-- Material form register -->
+                </section>
+            </div>
         </div>
     </div>
-</div>
+
+
+    {{--<div class="container my-6">--}}
+    {{--<div class="row justify-content-center">--}}
+    {{--<div class="col-md-8">--}}
+    {{--<section class="form-gradient">--}}
+    {{--<div class="card">--}}
+    {{--<div class="header pt-3 blue-gradient">--}}
+
+    {{--<div class="row d-flex justify-content-center">--}}
+    {{--<h3 class="h3-reponsive white-text mb-3 pt-3 font-weight-bold text-uppercase">{{ __('auth.register') }}</h3>--}}
+    {{--</div>--}}
+
+
+
+    {{--</div>--}}
+
+    {{--<div class="card-body mx-4 mt-4">--}}
+    {{--<form method="POST" action="{{ route('register') }}" aria-label="{{ __('auth.register') }}">--}}
+    {{--@csrf--}}
+    {{--<!--Body-->--}}
+    {{--<div class="md-form">--}}
+    {{--<input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username"--}}
+    {{--required autofocus> @if ($errors->has('username'))--}}
+    {{--<span class="invalid-feedback" role="alert">--}}
+    {{--<strong>{{ $errors->first('username') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--<label for="username">{{ __('auth.username') }}</label>--}}
+    {{--</div>--}}
+
+    {{--<div class="md-form">--}}
+    {{----}}
+    {{--<input type="text" id="Form-email3" class="form-control"> --}}
+    {{--<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"--}}
+    {{--required> @if ($errors->has('email'))--}}
+    {{--<span class="invalid-feedback" role="alert">--}}
+    {{--<strong>{{ $errors->first('email') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--<label for="email">{{ __('auth.email') }}</label>--}}
+
+    {{--</div>--}}
+
+    {{--<div class="md-form">--}}
+    {{--<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"--}}
+    {{--required> @if ($errors->has('password'))--}}
+    {{--<span class="invalid-feedback" role="alert">--}}
+    {{--<strong>{{ $errors->first('password') }}</strong>--}}
+    {{--</span>--}}
+    {{--@endif--}}
+    {{--<label for="password">{{ __('auth.password') }}</label>--}}
+    {{--</div>--}}
+
+    {{--<div class="md-form">--}}
+    {{--<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>--}}
+    {{--<label for="password-confirm">{{ __('auth.password-confirm') }}</label>--}}
+
+    {{--</div>--}}
+
+
+    {{--<!--Grid row-->--}}
+    {{--<div class="row d-flex align-items-center mb-4">--}}
+
+    {{--<!--Grid column-->--}}
+    {{--<div class="col-md-1 col-md-5 d-flex align-items-start">--}}
+    {{--<div class="text-center">--}}
+    {{--<button type="submit" class="btn btn-grey btn-rounded z-depth-1a">{{ __('auth.register-btn') }}</button>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<!--Grid column-->--}}
+
+    {{--<!--Grid column-->--}}
+    {{--<div class="col-md-7">--}}
+    {{--<p class="font-small grey-text d-flex justify-content-end mt-3">{{__('auth.user-exists')}}--}}
+    {{--<a href="{{route('login')}}" class="dark-grey-text ml-1 font-weight-bold">--}}
+    {{--{{ __('auth.login') }}</a>--}}
+    {{--</p>--}}
+    {{--</div>--}}
+    {{--<!--Grid column-->--}}
+
+    {{--</div>--}}
+    {{--<!--Grid row-->--}}
+    {{--</form>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</section>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 @endsection
