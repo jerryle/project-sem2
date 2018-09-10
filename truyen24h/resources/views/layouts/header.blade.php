64b9ac36ce08923be1aa9639c8d78fffab9f4f3c
jerryle <16341051+jerryle@users.noreply.github.com>
2018-09-10 23:47:26 +0700

    icon collapse

--- truyen24h/resources/views/layouts/header.blade.php
+++ truyen24h/resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2"
             aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <i class="fas fa-bars"></i>
+            <i class="fas fa-search"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent2">
 
