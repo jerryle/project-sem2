11a95404a83061d3292ce864353b5d76551571c7
ExamLaravel <noiltd00616@fpt.edu.vn>
2018-09-10 20:45:22 +0700

    edit

--- truyen24h/resources/views/auth/login.blade.php
+++ truyen24h/resources/views/auth/login.blade.php
@@ -4,9 +4,9 @@
     <div class="row mt-5">
         <!--Grid column-->
         <div class="col-md-6 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
-            <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Đăng nhập ngay! </h1>
-            <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
-            <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">Vui lòng đăng nhập để trải nghiệm và sử dụng đầy đủ chức năng của truyện24h - Đọc truyện online, đọc truyện chữ, truyện
+            <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.1s">Đăng nhập ngay! </h1>
+            <hr class="hr-light wow fadeInLeft" data-wow-delay="0.1s">
+            <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.1s">Vui lòng đăng nhập để trải nghiệm và sử dụng đầy đủ chức năng của truyện24h - Đọc truyện online, đọc truyện chữ, truyện
                 hay. Website luôn cập nhật những bộ truyện mới thuộc các thể loại đặc sắc như truyện tiên hiệp, truyện
                 kiếm hiệp, hay truyện ngôn tình một cách nhanh nhất. Hỗ trợ mọi thiết bị như di động và máy tính
                 bảng.</h6>
@@ -16,7 +16,7 @@
         <!--Grid column-->
         <div class="col-md-6 col-xl-5 mb-4">
             <!--Form-->
-            <div class="card wow fadeInRight" data-wow-delay="0.3s">
+            <div class="card wow fadeInRight" data-wow-delay="0.1s">
                 <div class="card-body">
                     <!--Header-->
                     <div class="text-center">
