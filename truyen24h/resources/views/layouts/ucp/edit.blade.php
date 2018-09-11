788afdc9d1b5855f467c118dd3d1e82cf81663d6
ExamLaravel <noiltd00616@fpt.edu.vn>
2018-09-11 18:28:44 +0700

    profile

---
+++ truyen24h/resources/views/layouts/ucp/edit.blade.php
@@ -0,0 +1,22 @@
+<form action="">
+    <div class="md-form">
+        <input type="text" id="name" class="form-control">
+        <label for="form1" >Tên</label>
+    </div>
+    <div class="md-form">
+        <input type="text" id="email" class="form-control">
+        <label for="form1" >Email</label>
+    </div>
+    <div class="md-form">
+        <input type="text" id="group" class="form-control">
+        <label for="form1" >Nhóm</label>
+    </div>
+    <div class="md-form">
+        <input type="text" id="birthday" class="form-control">
+        <label for="form1" >Ngày Sinh</label>
+    </div>
+    <button type="button" class="btn btn-blue-grey">Lưu Thông Tin</button>
+
+</form>
+
+
