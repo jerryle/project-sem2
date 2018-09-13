62a91d14c7bb63ec1a1246917d32a1648ee79360
ExamLaravel <noiltd00616@fpt.edu.vn>
2018-09-14 00:44:33 +0700

    update story

--- truyen24h/resources/views/pages/chapter.blade.php
+++ truyen24h/resources/views/pages/chapter.blade.php
@@ -6,100 +6,99 @@
 
 @endsection
 @section('content')
-<div class="row">
+    <div class="row">
 
 
-    <div class="container px-5">
-        <div class="media text-center text-lg-center">
-            <div class="media-body">
-                <a class="purple-text h2-responsive" href="#">{{$chapter->story->title}}</a>
-                <p>
+        <div class="container px-5">
+            <div class="media text-center text-lg-center">
+                <div class="media-body">
+                    <a class="purple-text h2-responsive" href="#">{{$chapter->story->title}}</a>
+                    <p>
                         <strong>Chương {{$chapter->number}}: {{$chapter->name}}</strong>
+                    </p>
+                    <p>
+                        Người đăng: <a href="#" class="text green-text">{{$chapter->user->username}}</a> - Lượt xem: {{$chapter->getViews()}}
+                    </p>
+                </div>
+            </div>
+            <div class="p-5" style="background: #eae7e7">
+
+                <p class="">
+
+                    @if($chapter->name !== 'None')
+                        {!!$chapter->content!!}
+                    @else
+                        Nội dung chương chưa được cập nhật
+                    @endif
                 </p>
-                <p>
-                    Người đăng: <a href="#" class="text green-text">{{$chapter->user->username}}</a> - Lượt xem: {{$chapter->getViews()}}
-                </p>
+
             </div>
         </div>
-        <div class="p-5" style="background: #eae7e7">
-
-            <p class="">
-
-                @if($chapter->name !== 'None')
-                {!!$chapter->content!!}
-                @else
-                Nội dung chương chưa được cập nhật
-                @endif
-            </p>
-
-        </div>
     </div>
-</div>
-<style type='text/css'>
-    #bttop {
-        border: 1px solid #4adcff;
-        background: #24bde2;
-        text-align: center;
-        padding: 5px;
-        position: fixed;
-        bottom: 35px;
-        right: 10px;
-        cursor: pointer;
-        display: none;
-        color: #fff;
-        font-size: 20px;
-        font-weight: 900;
-    }
-
-    #bttop:hover {
-        border: 1px solid #ffa789;
-        background: #737af4;
-    }
-</style>
-<div id='bttop'><i class="fas fa-arrow-alt-circle-up"></i></div>
-<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script>
-<script type='text/javascript'>
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() != 0) {
-                $('#bttop').fadeIn();
-            } else {
-                $('#bttop').fadeOut();
-            }
+    <style type='text/css'>
+        #bttop {
+            border: 1px solid #4adcff;
+            background: #24bde2;
+            text-align: center;
+            padding: 5px;
+            position: fixed;
+            bottom: 35px;
+            right: 10px;
+            cursor: pointer;
+            display: none;
+            color: #fff;
+            font-size: 20px;
+            font-weight: 900;
+        }
+        #bttop:hover {
+            border: 1px solid #ffa789;
+            background: #737af4;
+        }
+    </style>
+    <div id='bttop'><i class="fas fa-arrow-alt-circle-up"></i></div>
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js' type='text/javascript'></script>
+    <script type='text/javascript'>
+        $(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() != 0) {
+                    $('#bttop').fadeIn();
+                } else {
+                    $('#bttop').fadeOut();
+                }
+            });
+            $('#bttop').click(function () {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+            });
         });
-        $('#bttop').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-        });
-    });
-</script>
+    </script>
 
 
-{{--<div class="media mt-4 mb-2 px-1">--}}
+    {{--<div class="media mt-4 mb-2 px-1">--}}
     {{--<img class="card-img-100 d-flex z-depth-1 mr-3" src="{{$chapter->story->image}}" alt="{{$chapter->story->title}}">--}}
     {{--<div class="media-body">--}}
-        {{--<a class="text blue-text h2-responsive" href="#">{{$chapter->story->title}}</a>--}}
-        {{--<p>(Đang cập nhật)</p>--}}
+    {{--<a class="text blue-text h2-responsive" href="#">{{$chapter->story->title}}</a>--}}
+    {{--<p>(Đang cập nhật)</p>--}}
 
-        {{--<a class="fb-ic mr-3"><i class="fab fa-facebook-square"></i></a>--}}
+    {{--<a class="fb-ic mr-3"><i class="fab fa-facebook-square"></i></a>--}}
 
-        {{--<a class="tw-ic mr-3"><i class="fab fa-twitter-square"> </i></a>--}}
+    {{--<a class="tw-ic mr-3"><i class="fab fa-twitter-square"> </i></a>--}}
 
-        {{--<a class="gplus-ic mr-3"><i class="fab fa-google-plus-square"></i></a>--}}
-        {{--</div>--}}
+    {{--<a class="gplus-ic mr-3"><i class="fab fa-google-plus-square"></i></a>--}}
     {{--</div>--}}
-{{--<div class="detail mb-4">--}}
+    {{--</div>--}}
+    {{--<div class="detail mb-4">--}}
     {{--<i class="font-weight-bold">Chương {{$chapter->number}}: {{$chapter->name}}</i>--}}
     {{--
     <hr class="between-sections mt-0">--}}
     {{--<p class="text-left">--}}
-        {{--@if($chapter->name !== 'None')--}}
-        {{--{!!$chapter->content!!}--}}
-        {{--@else--}}
-        {{--Nội dung chương chưa được cập nhật--}}
-        {{--@endif--}}
-        {{--</p>--}}
+    {{--@if($chapter->name !== 'None')--}}
+    {{--{!!$chapter->content!!}--}}
+    {{--@else--}}
+    {{--Nội dung chương chưa được cập nhật--}}
+    {{--@endif--}}
+    {{--</p>--}}
 
     {{--</div>--}}
 
--- truyen24h/resources/views/pages/story.blade.php
+++ truyen24h/resources/views/pages/story.blade.php
@@ -6,7 +6,7 @@
                 <!--Grid row-->
                 <div class="row justify-content-center">
                     <!--Grid column-->
-                    <div class="col-md-4 table-responsive-sm">
+                    <div class="col-md-3 d-none d-sm-block table-responsive-sm">
                         <!--Featured image-->
                         <div class="view overlay rounded z-depth-2 waves-light mb-3">
                             <img src="{{$story->image}}" alt="" class="card-img img-fluid">
@@ -14,35 +14,34 @@
                     </div>
 
                     <!--Grid column-->
-                    <div class="col-md-8 table-responsive-sm">
-                        <h3 class="mb-4 font-weight-bold dark-grey-text">
-                            <strong>{{$story->title}}</strong>
-                        </h3>
+                    <div class="col-md-9 table-responsive-sm">
+                        <a class="purple-text h4-responsive" href="#"><strong>{{$story->title}}</strong></a>
                         <p>Người Đăng:
                             <a>
                                 <strong>{{$story->user->username}}</strong>
                             </a>, {{$story->created_at}}.</p>
-                        <p class="genre">Lượt xem: <strong>{{$story->getViews()}}</strong></p>
-                        <p>Thể Loại:
-                            <a>
-                                <strong>Kiếm Hiệp</strong>
-                            </a>
-                        </p>
-
+                        <button type="button" class="btn btn-outline-primary btn-rounded btn-sm waves-effect">Huyền Huyễn</button>
+                        <button type="button" class="btn btn-outline-primary btn-rounded btn-sm waves-effect">Kiếm Hiệp</button>
                         <p>Tác Giả:
                             <a>
                                 <strong>{{$story->author}}</strong>
                             </a>
                         </p>
+                        <button type="button" class="btn btn-outline-danger btn-sm waves-effect">Theo dõi</button>
+                        <button type="button" class="btn btn-outline-info btn-sm waves-effect">Số chương: </button>
+                        <button type="button" class="btn btn-outline-success btn-sm waves-effect">Lượt xem: <strong>{{$story->getViews()}}</strong></button>
 
-                        <p>
+                        <p>Giới thiệu:
                             @if($story->details != 'None')
                                 {{$story->details}}
+                                <a mdbBtn color="success" class="mb-3 waves-light">Read more ... </a>
                             @else
                                 Chưa có nội dung
                             @endif
                         </p>
-                        <a mdbBtn color="success" class="mb-3 waves-light">Read more ... </a>
+                        <div class="fb-share-button" data-href="https://truyen24h.site/home" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://truyen24h.site/home" class="fb-xfbml-parse-ignore">Chia sẻ</a></div>
+
+                        {{--<g:plusone size="medium" ></g:plusone>--}}
                     </div>
                     <!--Grid column-->
                 </div>
@@ -50,9 +49,9 @@
             </section>
             <section class="list-story">
                 <div class="row">
-                    <div class="col-md-8">
+                    <div class="col-md-8" style="background: #eae7e7">
                         <h3 class="text blue-text h2-responsive mt-1">Danh Sách Chương </h3>
-                        <div class="list-group list-group-flush">
+                        <div class="list-group list-group-flush" style="background: #eaeaea">
                             @foreach($chapters as $chapter)
                                 <a href="{{route('view_chapter',$chapter->getRouteKeyName())}}"
                                    class="list-group-item list-group-item-action waves-effect">
@@ -74,13 +73,16 @@
                             <div class="col-md-12 table-responsive-sm">
                                 <ul class="nav nav-tabs nav-justified" role="tablist">
                                     <li class="nav-item">
-                                        <a class="nav-link active" style="background: #f7f7f7" data-toggle="tab" href="#panel5" role="tab">Tuần</a>
+                                        <a class="nav-link active" style="background: #f7f7f7" data-toggle="tab"
+                                           href="#panel5" role="tab">Tuần</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" style="background: #f7f7f7" data-toggle="tab" href="#panel6" role="tab">Tháng</a>
+                                        <a class="nav-link" style="background: #f7f7f7" data-toggle="tab" href="#panel6"
+                                           role="tab">Tháng</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" style="background: #f7f7f7" data-toggle="tab" href="#panel7" role="tab">Năm</a>
+                                        <a class="nav-link" style="background: #f7f7f7" data-toggle="tab" href="#panel7"
+                                           role="tab">Năm</a>
                                     </li>
                                 </ul>
 
@@ -213,13 +215,22 @@
                     </div>
                 </div>
             </section>
-            <section class="full-story">
-                <h3 class="text blue-text h2-responsive mt-1">Truyện Cùng Thể Loại </h3>
-                <hr class="between-sections mt-0">
+            {{--<section class="full-story">--}}
+                {{--<h3 class="text blue-text h2-responsive mt-1">Truyện Cùng Thể Loại </h3>--}}
+                {{--<hr class="between-sections mt-0">--}}
 
-            </section>
+            {{--</section>--}}
 
         </div>
     </div>
+    <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
 
 @endsection
