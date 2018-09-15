ac0493d773b5f3b152dab677908d1f1fa55d92fe
jerryle <16341051+jerryle@users.noreply.github.com>
2018-09-15 12:56:16 +0700

    css active sidebar

--- truyen24h/resources/views/layouts/ucp/sidebar.blade.php
+++ truyen24h/resources/views/layouts/ucp/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="list-group">
+<div class="list-group t24-sidebar">
     <a href="#" class="list-group-item list-group-item-action text-uppercase font-weight-bold disabled">Truyện của bạn</a>
 <a href="{{route('user.story.follow_list')}}" class="list-group-item list-group-item-action waves-effect {{$current_menu == 'follow-list' ? 'active' : ''}}">Danh sách theo dõi</a>
 <a href="{{route('user.story.list')}}" class="list-group-item list-group-item-action waves-effect {{$current_menu == 'list' ? 'active' : ''}}">Truyện đã đăng</a>
